6594a211d81ce7733c4844a0b125ff69b38a9886
Noonenowhoiam <142042473+Noonenowhoiam@users.noreply.github.com>
2023-12-18 20:06:28 +0600

    Update RegisteredReforges.java

--- src/main/java/me/CarsCupcake/SkyblockRemake/Items/reforges/RegisteredReforges.java
+++ src/main/java/me/CarsCupcake/SkyblockRemake/Items/reforges/RegisteredReforges.java
@@ -2,45 +2,53 @@ package me.CarsCupcake.SkyblockRemake.Items.reforges;
 
 import java.util.HashMap;
 
-import me.CarsCupcake.SkyblockRemake.Items.reforges.ReforgeStones.Auspicious;
-import me.CarsCupcake.SkyblockRemake.Items.reforges.ReforgeStones.Jaded;
-import me.CarsCupcake.SkyblockRemake.Items.reforges.ReforgeStones.Loving;
-import me.CarsCupcake.SkyblockRemake.Items.reforges.ReforgeStones.Necrotic;
+import me.CarsCupcake.SkyblockRemake.Items.reforges.ReforgeStones.*;
 import me.CarsCupcake.SkyblockRemake.Items.reforges.blacksmith.Epic;
+import me.CarsCupcake.SkyblockRemake.Items.reforges.blacksmith.Hasty;
 import me.CarsCupcake.SkyblockRemake.Items.reforges.blacksmith.Heroic;
-
+import me.CarsCupcake.SkyblockRemake.Items.reforges.blacksmith.Rapid;
 
 
 public class RegisteredReforges {
 	public static HashMap<String, Reforge> reforges = new HashMap<>();
 	public static HashMap<String, Reforge> blacksmith_reforges = new HashMap<>();
-	
+
 	public static HashMap<String, Reforge> reforgeStones_reforges = new HashMap<>();
-public static void init() {
-	initBlacksmith();
-	initReforgeStones();
-}
-private static void initBlacksmith() {
-	blacksmith_reforges.put("Epic", new Epic());
-	reforges.put("Epic", new Epic());
-	
-	blacksmith_reforges.put("Heroic", new Heroic());
-	reforges.put("Heroic", new Heroic());
-}
 
-private static void initReforgeStones() {
-	reforgeStones_reforges.put("Necrotic", new Necrotic());
-	reforges.put("Necrotic", new Necrotic());
-	
-	reforgeStones_reforges.put("Loving", new Loving());
-	reforges.put("Loving", new Loving());
+	public static void init() {
+		initBlacksmith();
+		initReforgeStones();
+	}
 
-	reforgeStones_reforges.put("Auspicious", new Auspicious());
-	reforges.put("Auspicious", new Auspicious());
+	private static void initBlacksmith() {
+		blacksmith_reforges.put("Epic", new Epic());
+		reforges.put("Epic", new Epic());
 
-	reforgeStones_reforges.put("Jaded", new Jaded());
-	reforges.put("Jaded", new Jaded());
-	
-}
+		blacksmith_reforges.put("Heroic", new Heroic());
+		reforges.put("Heroic", new Heroic());
+
+		blacksmith_reforges.put("Rapid", new Rapid());
+		reforges.put("Rapid", new Rapid());
+
+		blacksmith_reforges.put("Hasty", new Hasty());
+		reforges.put("Hasty", new Hasty());
+	}
+
+	private static void initReforgeStones() {
+		reforgeStones_reforges.put("Necrotic", new Necrotic());
+		reforges.put("Necrotic", new Necrotic());
+
+		reforgeStones_reforges.put("Loving", new Loving());
+		reforges.put("Loving", new Loving());
+
+		reforgeStones_reforges.put("Auspicious", new Auspicious());
+		reforges.put("Auspicious", new Auspicious());
+
+		reforgeStones_reforges.put("Jaded", new Jaded());
+		reforges.put("Jaded", new Jaded());
+
+		reforgeStones_reforges.put("Precise", new Precise());
+		reforges.put("Precise", new Precise());
+	}
 
 }
